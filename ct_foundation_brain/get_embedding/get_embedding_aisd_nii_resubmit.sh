d57e09e2516a4c716bce4e0e40d38e38cec6ece4
Dufan Wu <wudufan33@gmail.com>
2025-02-11 03:58:57 +0000

    get embedding aisd

---
+++ ct_foundation_brain/get_embedding/get_embedding_aisd_nii_resubmit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script is used to get the embedding of the AISD dataset
+
+output_dir="aisd/embeddings/origin"
+bucket_dir="AISD/origin"
+project_id="fleet-space-445215-f7"
+bucket_name="ct_hemorrhage"
+parallel_size="8"
+
+root_dir="/mnt/data"
+
+mkdir -p "${root_dir}/${output_dir}"
+
+python get_embedding_from_nii_resubmit.py \
+    --output_dir "${output_dir}" \
+    --bucket_dir "${bucket_dir}" \
+    --project_id "${project_id}" \
+    --bucket_name "${bucket_name}" \
+    --parallel_size "${parallel_size}" \
+    &> "${root_dir}/${output_dir}/get_embedding_aisd_nii_resubmit_20250210.log"
